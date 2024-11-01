33417ec308042c9c33754bae380555f1800b9527
JongbeomLee623 <jambon23@dgu.ac.kr>
2024-11-01 16:12:56 +0900

    BE: [fix] 관리자 entity 제약조건 수정 #1

--- src/backend/Eyesee/src/main/java/com/fortune/eyesee/entity/Admin.java
+++ src/backend/Eyesee/src/main/java/com/fortune/eyesee/entity/Admin.java
@@ -13,7 +13,12 @@ public class Admin {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer adminId;
 
+    @Column(nullable = false, unique = true) // 이메일 중복 방지
     private String adminEmail;
+
+    @Column(nullable = false, length = 255) // 비밀번호 암호화 후 저장 시 충분한 크기로 설정
     private String password;
+
+    @Column(nullable = false, length = 100)
     private String adminName;
 }

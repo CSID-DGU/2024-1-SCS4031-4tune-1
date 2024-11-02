1887660105727019a88413b0ddd61d5817486753
Hyeona01 <sha0209@dgu.ac.kr>
2024-11-02 17:14:54 +0900

    feat: 초기 페이지 구성 #9

---
+++ src/frontend/eyesee-admin/src/app/(home)/page.tsx
@@ -0,0 +1,13 @@
+// import Logo from ;
+
+import HomeNav from "@/components/home/HomeNav";
+
+const HomePage = () => {
+  return (
+    <div className="bg-bgGradient w-screen h-screen">
+      <HomeNav />
+    </div>
+  );
+};
+
+export default HomePage;

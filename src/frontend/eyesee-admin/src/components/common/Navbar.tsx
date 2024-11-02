ebf3d5d67ce4a34e93c7f974a5d077c7d481d644
Hyeona01 <sha0209@dgu.ac.kr>
2024-11-03 00:24:10 +0900

    feat: 공용 컴포넌트 헤더 추가 #11

---
+++ src/frontend/eyesee-admin/src/components/common/Navbar.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Logo from "@/assets/images/logo.svg";
+import Profile from "@/assets/icons/Profile.svg";
+
+type NavbarProps = {
+  bgColr: string;
+};
+
+const Navbar = ({ bgColr }: NavbarProps) => {
+  const router = useRouter();
+
+  const handleNavigation = (path: string) => {
+    router.push(path);
+  };
+
+  return (
+    <div className={`${bgColr} flex h-32 items-center justify-between px-12`}>
+      <div>
+        <Logo />
+      </div>
+      <div className="flex justify-between gap-16">
+        <button className="text-2xl text-white">About us</button>
+        <Profile />
+        {/* <button
+          onClick={() => handleNavigation("/signin")}
+          className="text-2xl"
+        >
+          Sign in
+        </button> */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

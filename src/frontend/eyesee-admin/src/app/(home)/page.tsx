d28c5ecef62e5e8b3ed71ccb9497dee0436bc2c0
Hyeona01 <sha0209@dgu.ac.kr>
2024-11-02 18:44:28 +0900

    feat: 랜딩페이지 퍼블리싱 및 라우터 설정 #11

--- src/frontend/eyesee-admin/src/app/(home)/page.tsx
+++ src/frontend/eyesee-admin/src/app/(home)/page.tsx
@@ -1,9 +1,11 @@
+import HomeContents from "@/components/home/HomeContents";
 import HomeNav from "@/components/home/HomeNav";
 
 const HomePage = () => {
   return (
-    <div className="bg-bgGradient w-screen h-screen">
+    <div className="bg-bgGradient w-screen h-screen flex flex-col justify-between">
       <HomeNav />
+      <HomeContents />
     </div>
   );
 };

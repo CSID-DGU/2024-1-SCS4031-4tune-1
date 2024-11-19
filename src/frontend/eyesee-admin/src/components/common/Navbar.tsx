3ee623a086cc70813f658e740ed67b2357f77f4f
Hyeona01 <sha0209@dgu.ac.kr>
2024-11-19 16:51:43 +0900

    FE: [feat] 로그인 여부에 따른 nav 렌더링 #40

--- src/frontend/eyesee-admin/src/components/common/Navbar.tsx
+++ src/frontend/eyesee-admin/src/components/common/Navbar.tsx
@@ -3,18 +3,28 @@
 import { useRouter } from "next/navigation";
 import Logo from "@/assets/images/logo.svg";
 import Profile from "@/assets/icons/Profile.svg";
+import { useEffect, useState } from "react";
+import { getAccessToken } from "@/utils/auth";
 
 type NavbarProps = {
   bgColr: string;
-  hasMenu?: boolean;
+  hasMenu: boolean;
 };
 
 const Navbar = ({ bgColr, hasMenu }: NavbarProps) => {
   const router = useRouter();
-
   const handleNavigation = (path: string) => {
     router.push(path);
   };
+
+  const [isSignin, setIsSignin] = useState(false);
+
+  // 로그인 여부 확인
+  useEffect(() => {
+    if (getAccessToken()) {
+      setIsSignin(true);
+    }
+  }, []);
 
   return (
     <div
@@ -27,13 +37,16 @@ const Navbar = ({ bgColr, hasMenu }: NavbarProps) => {
         {hasMenu && (
           <>
             <button className="text-2xl text-white">About us</button>
-            <Profile />
-            {/* <button
-          onClick={() => handleNavigation("/signin")}
-          className="text-2xl"
-        >
-          Sign in
-        </button> */}
+            {isSignin ? (
+              <Profile onClick={() => router.push("/mypage")} />
+            ) : (
+              <button
+                onClick={() => handleNavigation("/signin")}
+                className="text-2xl text-white"
+              >
+                Sign in
+              </button>
+            )}
           </>
         )}
       </div>

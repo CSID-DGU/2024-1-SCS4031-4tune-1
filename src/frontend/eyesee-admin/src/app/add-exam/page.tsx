deac525fe7a28d5c185023ae8dee865877340076
Hyeona01 <sha0209@dgu.ac.kr>
2024-11-19 17:49:14 +0900

    FE: [feat] 서브 헤더 추가 #41

--- src/frontend/eyesee-admin/src/app/add-exam/page.tsx
+++ src/frontend/eyesee-admin/src/app/add-exam/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import AddExamHeader from "@/components/add-exam/AddExamHeader";
+import SubHeader from "@/components/add-exam/SubHeader";
 import { useState } from "react";
 
 const AddExamPage = () => {
   const [step, setStep] = useState(1);
 
   return (
-    <div>
+    <div className="px-20">
       <AddExamHeader step={step} />
+      <SubHeader title="강의 정보를 입력해주세요" />
     </div>
   );
 };
--- src/frontend/eyesee-admin/src/components/add-exam/AddExamHeader.tsx
+++ src/frontend/eyesee-admin/src/components/add-exam/AddExamHeader.tsx
@@ -4,46 +4,62 @@ type AddExamHeaderProps = {
 
 const AddExamHeader = ({ step }: AddExamHeaderProps) => {
   return (
-    <div className="pb-[12vh] border-b-2 border-[#0E1D3C]">
+    <div className="pb-[100px] border-b-[3px] border-[#0E1D3C]">
       <div className="relative bg-[#999] h-[2px] w-full my-10">
-        <div className="absolute bg-[#0E1D3C] rounded-full w-6 h-6 top-[-12px]" />
-        <div className="absolute bg-[#0E1D3C] rounded-full w-6 h-6 top-[-12px] left-[33%]" />
-        <div className="absolute bg-[#0E1D3C] rounded-full w-6 h-6 top-[-12px] left-[66%]" />
-        <div className="absolute bg-[#0E1D3C] rounded-full w-6 h-6 top-[-12px] right-0" />
-      </div>
-      <div
-        className={`absolute left-0 flex flex-col items-start justify-center ${
-          step === 1 ? "text-[#0E1D3C]" : "text-[#999]"
-        } text-[28px] font-bold`}
-      >
-        <div>STEP1.</div>
-        <div>강의 정보 등록</div>
-      </div>
-      <div
-        className={`absolute left-[33%] flex flex-col items-start ${
-          step === 2 ? "text-[#0E1D3C]" : "text-[#999]"
-        } text-[28px] font-bold`}
-      >
-        <div>STEP2.</div>
-        <div>시험 정보 등록</div>
-      </div>
-      <div
-        className={`absolute left-[66%] flex flex-col items-start ${
-          step === 3 ? "text-[#0E1D3C]" : "text-[#999]"
-        } text-[28px] font-bold`}
-      >
-        <div>STEP3.</div>
-        <div>유의사항 등록</div>
-      </div>
-      <div
-        className={`absolute right-0 flex flex-col items-end ${
-          step === 4 ? "text-[#0E1D3C]" : "text-[#999]"
-        } text-[28px] font-bold`}
-      >
-        <div>
-          사전정보
-          <br />
-          등록 완료
+        {/* 스탬프 영역 */}
+        <div
+          className={`absolute bg-[#0E1D3C] rounded-full w-6 h-6 top-[-12px]`}
+        />
+        <div
+          className={`absolute ${
+            step === 1 ? "bg-[#999]" : "bg-[#0E1D3C]"
+          } rounded-full w-6 h-6 top-[-12px] left-[33%]`}
+        />
+        <div
+          className={`absolute ${
+            step <= 2 ? "bg-[#999]" : "bg-[#0E1D3C]"
+          } rounded-full w-6 h-6 top-[-12px] left-[66%]`}
+        />
+        <div
+          className={`absolute  ${
+            step !== 4 ? "bg-[#999]" : "bg-[#0E1D3C]"
+          } rounded-full w-6 h-6 top-[-12px] right-0`}
+        />
+        {/* 글자 영역 */}
+        <div
+          className={`absolute top-[30px] flex flex-col items-start justify-center ${
+            step === 1 ? "text-[#0E1D3C]" : "text-[#999]"
+          } text-[28px] font-bold`}
+        >
+          <div>STEP1.</div>
+          <div>강의 정보 등록</div>
+        </div>
+        <div
+          className={`absolute top-[30px] left-[33%] flex flex-col items-start ${
+            step === 2 ? "text-[#0E1D3C]" : "text-[#999]"
+          } text-[28px] font-bold`}
+        >
+          <div>STEP2.</div>
+          <div>시험 정보 등록</div>
+        </div>
+        <div
+          className={`absolute top-[30px] left-[66%] flex flex-col items-start ${
+            step === 3 ? "text-[#0E1D3C]" : "text-[#999]"
+          } text-[28px] font-bold`}
+        >
+          <div>STEP3.</div>
+          <div>유의사항 등록</div>
+        </div>
+        <div
+          className={`absolute top-[30px] right-0 flex flex-col ${
+            step === 4 ? "text-[#0E1D3C]" : "text-[#999]"
+          } text-[28px] font-bold`}
+        >
+          <div>
+            사전정보
+            <br />
+            등록 완료
+          </div>
         </div>
       </div>
     </div>

f87dec06a5f8b5c6b9680aabf79f734959a48088
Hyeona01 <sha0209@dgu.ac.kr>
2024-11-19 22:45:24 +0900

    FE: [feat] 시험정보입력 STEP3,4 추가 #41

--- src/frontend/eyesee-admin/src/app/add-exam/page.tsx
+++ src/frontend/eyesee-admin/src/app/add-exam/page.tsx
@@ -3,17 +3,23 @@
 import AddExamHeader from "@/components/add-exam/AddExamHeader";
 import Step1 from "@/components/add-exam/Step1";
 import Step2 from "@/components/add-exam/Step2";
+import Step3 from "@/components/add-exam/Step3";
+import Step4 from "@/components/add-exam/Step4";
 import SubHeader from "@/components/add-exam/SubHeader";
 import NextButton from "@/components/common/NextButton";
 import { ExamRequest, initialExamData } from "@/types/postExam";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const AddExamPage = () => {
+  const router = useRouter();
   const [step, setStep] = useState(1);
   const [examData, setExamData] = useState<ExamRequest>(initialExamData);
 
   const handleSubmit = () => {
-    setStep(step + 1);
+    if (step === 4) {
+      router.push("/");
+    } else setStep(step + 1);
     console.log(
       examData.examDate,
       examData.examDuration,
@@ -24,9 +30,19 @@ const AddExamPage = () => {
   return (
     <div className="px-20">
       <AddExamHeader step={step} />
-      <SubHeader title="강의 정보를 입력해주세요" />
+      {step !== 4 && (
+        <SubHeader
+          title={
+            step === 1
+              ? "강의 정보를 입력해주세요"
+              : step === 2
+              ? "시험 정보를 입력해주세요"
+              : "수험자에게 전달할 유의사항을 입력해주세요"
+          }
+        />
+      )}
       {/* 입력 필드 */}
-      <div className="mt-20 flex flex-col items-center w-screen">
+      <div className="mt-20 flex flex-col items-center w-full">
         <div className="w-[1000px]">
           {step === 1 && (
             <Step1 examData={examData} setExamData={setExamData} />
@@ -35,14 +51,12 @@ const AddExamPage = () => {
             <Step2 examData={examData} setExamData={setExamData} />
           )}
           {step === 3 && (
-            <Step1 examData={examData} setExamData={setExamData} />
+            <Step3 examData={examData} setExamData={setExamData} />
           )}
-          {step === 4 && (
-            <Step1 examData={examData} setExamData={setExamData} />
-          )}
+          {step === 4 && <Step4 code="2e1431" />}
         </div>
       </div>
-      <NextButton title="NEXT" onClick={handleSubmit} />
+      <NextButton title={step !== 4 ? "NEXT" : "HOME"} onClick={handleSubmit} />
     </div>
   );
 };
---
+++ src/frontend/eyesee-admin/src/components/add-exam/Step3.tsx
@@ -0,0 +1,29 @@
+import { ExamRequest } from "@/types/postExam";
+import { Dispatch, SetStateAction } from "react";
+
+type Step3Props = {
+  examData: ExamRequest;
+  setExamData: Dispatch<SetStateAction<ExamRequest>>;
+};
+
+const Step3 = ({ examData, setExamData }: Step3Props) => {
+  return (
+    <div>
+      {/* 유의사항 */}
+      <div>
+        <label className="text-black text-[24px] mb-3 block">유의사항</label>
+        <textarea
+          rows={10}
+          value={examData.examNotice}
+          onChange={(e) =>
+            setExamData({ ...examData, examNotice: e.target.value })
+          }
+          placeholder="유의사항을 자유롭게 입력해주세요"
+          className="w-full pb-20 bg-[#f3f3f3] rounded-md border border-[#A0A0A0] py-5 px-8 text-black text-[20px] resize-none"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Step3;

0101506ffdb29e5935800c8452075aded52d4c65
천기정 <chun@cheongijeong-ui-MacBookAir.local>
2024-11-26 21:20:32 +0900

    AI: [fix] modify directory

--- src/ai/4tune_AI/app/utils/imaga_processing.py
+++
@@ -1,8 +0,0 @@
-# app/utils/image_processing.py
-import cv2
-import numpy as np
-
-def read_image_from_bytes(image_bytes):
-    np_arr = np.frombuffer(image_bytes, np.uint8)
-    image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-    return image

01d36837efc240006f143fd8e1df9d60ae17941f
Hyeona01 <sha0209@dgu.ac.kr>
2024-11-27 15:42:28 +0900

    FE: [feat] 대시보드 데이터 3초간격으로 리프레시 #66

--- src/frontend/eyesee-admin/src/hooks/api/useDashboard.ts
+++ src/frontend/eyesee-admin/src/hooks/api/useDashboard.ts
@@ -5,5 +5,7 @@ export const useDashboardData = (examId: number) => {
   return useQuery({
     queryKey: ["dashboard", examId],
     queryFn: () => getDashboardData(examId),
+    refetchInterval: 3000, // 3초 간격으로 리프레시
+    refetchIntervalInBackground: true, // 화면이 포커스되지 않아도 백그라운드에서 리프레시
   });
 };

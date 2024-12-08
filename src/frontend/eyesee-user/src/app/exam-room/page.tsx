ff4e08f895f4af9aa6e3f04c26b404a2a4e29b03
Hyeona01 <sha0209@dgu.ac.kr>
2024-12-08 14:05:31 +0900

    FE: [fix] 빌드 에러 해결

--- src/frontend/eyesee-user/src/app/exam-room/page.tsx
+++ src/frontend/eyesee-user/src/app/exam-room/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import NextButton from "@/components/common/NextButton";
 import { useUserStore } from "@/store/useUserStore";
 import { useStore } from "@/store/useStore";

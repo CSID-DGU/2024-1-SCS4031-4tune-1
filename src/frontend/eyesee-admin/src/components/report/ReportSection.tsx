3b3340b7173d5e5fea62b233fe3eff610bb6c3ee
Hyeona01 <sha0209@dgu.ac.kr>
2024-12-08 23:58:14 +0900

    FE: [fix] 레포트 데이터 소수점 처리

--- src/frontend/eyesee-admin/src/components/report/ReportSection.tsx
+++ src/frontend/eyesee-admin/src/components/report/ReportSection.tsx
@@ -37,33 +37,33 @@ const ReportSection = ({ reportData }: ReportSectionType) => {
         <p>부정행위 탐지 결과</p>
       </div>
       <div className="[w-50%] flex flex-col h-full">
-        <div className="w-full flex gap-20 py-3 items-center justify-between text-[20px] text-bold border-b border-white">
+        <div className="w-full flex gap-10 py-3 items-center justify-between text-[20px] text-bold border-b border-white">
           <div>총 탐지된 부정행위 건수</div>
           <div>{reportData.totalCheatingCount}건</div>
         </div>
-        <div className="w-full flex gap-20 py-3 items-center justify-between text-[20px] text-bold border-b border-white">
+        <div className="w-full flex gap-10 py-3 items-center justify-between text-[20px] text-bold border-b border-white">
           <div>부정행위 탐지 수험자 수</div>
           <div>{reportData.cheatingStudentsCount}명</div>
         </div>
-        <div className="w-full flex gap-20 py-3 items-center justify-between text-[20px] text-bold border-b border-white">
+        <div className="w-full flex gap-10 py-3 items-center justify-between text-[20px] text-bold border-b border-white">
           <div>평균 부정행위 탐지 건수</div>
-          <div>{reportData.averageCheatingCount}건/수험자</div>
+          <div>{reportData.averageCheatingCount.toFixed(1)}건/수험자</div>
         </div>
-        <div className="w-full flex gap-20 py-3 items-center justify-between text-[20px] text-bold border-b border-white">
+        <div className="w-full flex gap-10 py-3 items-center justify-between text-[20px] text-bold border-b border-white">
           <div>최다 부정행위 탐지 수험자</div>
           <div>{reportData.maxCheatingStudent}</div>
         </div>
-        <div className="w-full flex gap-20 py-3 items-center justify-between text-[20px] text-bold border-b border-white">
+        <div className="w-full flex gap-10 py-3 items-center justify-between text-[20px] text-bold border-b border-white">
           <div>부정행위 탐지율</div>
-          <div>{reportData.cheatingRate}%</div>
+          <div>{reportData.cheatingRate.toFixed(2)}%</div>
         </div>
-        <div className="w-full flex gap-20 py-3 items-start justify-between text-[20px] text-bold border-b border-white">
+        <div className="w-full flex gap-10 py-3 items-start justify-between text-[20px] text-bold border-b border-white">
           <div>부정행위 유형별 통계</div>
           <div className="flex flex-col gap-2">
             {Object.entries(reportData.cheatingTypeStatistics).map(
               ([type, count]) => (
-                <span key={type} className="border-b border-gray-500">
-                  {type}: {count}건{", "}
+                <span key={type} className="border-b border-gray-500 px-3">
+                  {type}: {count}건
                 </span>
               )
             )}
